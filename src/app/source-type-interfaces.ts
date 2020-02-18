2d5efc464475cccd35256421196d02a35cbb0f2e
cKoert <christian.koert@outlook.com>
2020-02-18 12:18:05 +0100

    Create interfaces for every DataEnvelope

---
+++ src/app/source-type-interfaces.ts
@@ -0,0 +1,18 @@
+export interface SensorWeb {
+  name: "SensorWeb";
+}
+
+export interface Copernicus {
+  name: "Copernicus";
+}
+
+export interface GdiDe {
+  name: "GDI-DE";
+}
+
+export interface Dwd {
+  name: "DWD";
+}
+export interface WacodisProduct {
+  name: "WacodisProduct";
+}
